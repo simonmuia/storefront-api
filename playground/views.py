287977088911320e4b7483b00e572ccec18e5ac2
Simon Muia <simonmuia38@gmail.com>
2023-02-01 12:07:47 +0300

    Debugging Django App

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
+
 # Create your views here.
+def calculate():
+   x=1
+   y= 2
+   return x
+
+
 
 def say_hello(request):
-   return  render(request, 'hello.html')
+   x=calculate()
+   y=2
+   return  render(request, 'hello.html',{"name":"Simon"})
